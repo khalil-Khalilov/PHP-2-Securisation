3f88c484351dc53a3f16d447e2a1b48bc2d12b35
Khalilov Khalil <xelil.khalilov@gmail.com>
2022-10-02 10:47:30 +0200

    all criteria are met

--- index.php
+++ index.php
@@ -33,7 +33,9 @@
             justify-content: center;
         }
 
-        label, textarea, input:last-child{
+        label,
+        textarea,
+        input:last-child {
             margin-top: 1rem;
         }
     </style>
@@ -46,19 +48,19 @@
         <form action="thanks.php" method="POST">
 
             <label for="users_last-name">Nom :</label>
-            <input type="text" id="users_last-name" name="users_last-name" for="users_last-name">
+            <input type="text" id="users_last-name" name="users_last-name" for="users_last-name" required>
 
             <label for="users_first-name">Prenom :</label>
-            <input type="text" id="users_first-name" name="users_first-name" for="users_first-name">
+            <input type="text" id="users_first-name" name="users_first-name" for="users_first-name" required>
 
             <label for="users_email">Email :</label>
-            <input type="email" id="users_email" name="users_email" for="users_email" placeholder="exemple@gmail.com">
+            <input type="email" id="users_email" name="users_email" for="users_email" placeholder="exemple@gmail.com" required>
 
             <label for="users_phone">Numéro de téléphone :</label>
-            <input type="tel" id="users_phone" name="users_phone" for="users_phone">
+            <input type="tel" id="users_phone" name="users_phone" for="users_phone" required>
 
             <label for="topics">Sujets :</label>
-            <select name="topics" id="topics">
+            <select name="topics" id="topics" required>
                 <option disabled selected value> -- sélectionner une option -- </option>
                 <option value="J'ai oublié mon mot de passe">J'ai oublié mon mot de passe</option>
                 <option value="Mon compte a été volé">Mon compte a été volé</option>
@@ -68,14 +70,12 @@
             </select>
 
 
-            <textarea id="users_message" name="users_message" for="users_message" cols="60" rows="10"></textarea>
+            <textarea id="users_message" name="users_message" for="users_message" cols="60" rows="10" required></textarea>
 
             <input type="submit" value="ENVOYER">
         </form>
 
     </div>
-
-
 </body>
 
 </html>
--- thanks.php
+++ thanks.php
@@ -1,3 +1,19 @@
+<?php
+$required = "required";
+
+if (!empty($_POST['users_last-name']) && !empty($_POST['users_first-name']) && !empty($_POST['users_email']) && 
+!empty($_POST['users_phone']) && !empty($_POST['topics']) && !empty($_POST['users_message'])) {
+
+    $firstName = htmlspecialchars(trim($_POST['users_first-name']));
+    $lastName = htmlspecialchars(trim($_POST['users_last-name']));
+    $usersEmail = htmlspecialchars(trim(filter_var($_POST['users_email'], FILTER_VALIDATE_EMAIL)));
+    $usersPhone = (int)htmlspecialchars(trim($_POST['users_phone']));
+    $topics = htmlspecialchars(trim($_POST['topics']));
+    $usersMessage = htmlspecialchars(trim($_POST['users_message']));
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -5,7 +21,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
     <style>
         * {
             margin: 0;
@@ -28,9 +44,9 @@
 
 <body>
     <a href="index.php">Return</a>
-    <p> Merci <?= $_POST['users_first-name']; ?> <?= $_POST['users_last-name']; ?> de nous avoir contacté à propos de “<?= $_POST['topics']; ?>”.
-        Un de nos conseiller vous contactera soit à l’adresse <?= $_POST['users_email']; ?> ou par téléphone au <?= $_POST['users_phone']; ?>
-        dans les plus brefs délais pour traiter votre demande : <?= $_POST['users_message']; ?>
+    <p> Merci <?=$firstName?> <?= $lastName; ?> de nous avoir contacté à propos de “<?= $topics; ?>”.
+        Un de nos conseiller vous contactera soit à l’adresse <?= $usersEmail; ?> ou par téléphone au <?= $usersPhone; ?>
+        dans les plus brefs délais pour traiter votre demande : <?= $usersMessage; ?>
     </p>
 </body>
 
